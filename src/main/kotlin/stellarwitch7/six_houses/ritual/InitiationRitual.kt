97d90bdd8d64eab04c0007a262a3f6fc36a2dd0f
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-09-02 19:38:10 -0400

    Sigh...

--- src/main/kotlin/stellarwitch7/six_houses/ritual/InitiationRitual.kt
+++ src/main/kotlin/stellarwitch7/six_houses/ritual/InitiationRitual.kt
@@ -2,6 +2,7 @@ package stellarwitch7.six_houses.ritual
 
 import net.minecraft.entity.EntityType
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.sound.SoundEvents
 import net.minecraft.util.math.BlockPos
 import stellarwitch7.libstellar.registry.codec.CodecType
 import stellarwitch7.libstellar.ritual.Ritual
@@ -31,7 +32,9 @@ class InitiationRitual(pos: BlockPos, steps: ArrayDeque<Step>) : Ritual(pos, ste
             ClearCandleStep(BlockPos(2, 0, -2)),
             SleepStep(20),
             ConsumeItemStep(dev.enjarai.trickster.item.ModItems.TOME_OF_TOMFOOLERY, 2.0),
-            DropItemStep(ModItems.grimoire.defaultStack, BlockPos(0, 1, 0))
+            DropItemStep(ModItems.grimoire.defaultStack, BlockPos(0, 1, 0)),
+            //TODO: it don't work?
+            PlaySoundStep(SoundEvents.BLOCK_CHEST_OPEN, BlockPos(0, 1, 0), 1F, 1F)
         ))
 
         return this
